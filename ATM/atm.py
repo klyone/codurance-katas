3709c0cf05c268dd6508b5f568b9849536cef51a
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2023-01-23 13:28:44 +0100

    ATM: Create an ATM class and refactor the rest of the code.

--- ATM/atm.py
+++ ATM/atm.py
@@ -1,67 +1,71 @@
 #!/usr/bin/env python3
 
-def is_bill(bill_coin):
-    if bill_coin > 2:
-        return True
-    else:
-        return False
+class ATM:
+    def __init__(self):
+        pass
 
-def get_money_type(money, amount):
-    money_type = "bill"
+    def is_bill(bill_coin):
+        if bill_coin > 2:
+            return True
+        else:
+            return False
 
-    if not is_bill(money):
-        money_type = "coin"
+    def get_money_type(money, amount):
+        money_type = "bill"
 
-    if amount > 1:
-        money_type = money_type + "s"
+        if not ATM.is_bill(money):
+            money_type = "coin"
 
-    return money_type
+        if amount > 1:
+            money_type = money_type + "s"
 
-
-def count_bill_or_coin_for(bill_coin, money):
-    count = 0
-
-    for m in money:
-        if m == bill_coin:
-            count = count + 1
-
-    return count
+        return money_type
 
 
-def find_largest_bill_or_coin_for(amount):
-    allowed_money = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+    def count_bill_or_coin_for(bill_coin, money):
+        count = 0
 
-    for money in allowed_money:
-        if amount >= money:
-            break
+        for m in money:
+            if m == bill_coin:
+                count = count + 1
 
-    return money
+        return count
 
 
-def withdraw(amount):
-    withdraw_money = []
+    def find_largest_bill_or_coin_for(amount):
+        allowed_money = [500, 200, 100, 50, 20, 10, 5, 2, 1]
 
-    while amount > 0:
-        money = find_largest_bill_or_coin_for(amount)
-        amount = amount - money
-        withdraw_money.append(money)
+        for money in allowed_money:
+            if amount >= money:
+                break
 
-    return withdraw_money
+        return money
 
-def print_withdraw(amount):
-    withdraw_money = withdraw(amount)
-    previous_money = -1
-    withdraw_string = ""
 
-    for money in withdraw_money:
-        if previous_money == money:
-            continue
+    def withdraw(self, amount):
+        withdraw_money = []
 
-        quantity = count_bill_or_coin_for(money, withdraw_money)
-        money_type = get_money_type(money, quantity)
-        withdraw_line = f"{quantity} {money_type} of {money}.\n"
-        withdraw_string = withdraw_string + withdraw_line
+        while amount > 0:
+            money = ATM.find_largest_bill_or_coin_for(amount)
+            amount = amount - money
+            withdraw_money.append(money)
 
-        previous_money = money
+        return withdraw_money
 
-    return withdraw_string
+    def print_withdraw(self, amount):
+        withdraw_money = self.withdraw(amount)
+        previous_money = -1
+        withdraw_string = ""
+
+        for money in withdraw_money:
+            if previous_money == money:
+                continue
+
+            quantity = ATM.count_bill_or_coin_for(money, withdraw_money)
+            money_type = ATM.get_money_type(money, quantity)
+            withdraw_line = f"{quantity} {money_type} of {money}.\n"
+            withdraw_string = withdraw_string + withdraw_line
+
+            previous_money = money
+
+        return withdraw_string
--- ATM/test_atm.py
+++ ATM/test_atm.py
@@ -7,16 +7,21 @@ def test_nothing():
     pass
 
 def test_withdraw_200_usd():
-    assert withdraw(200) == [200]
+    atm = ATM()
+    assert atm.withdraw(200) == [200]
 
 def test_withdraw_100_usd():
-    assert withdraw(100) == [100]
+    atm = ATM()
+    assert atm.withdraw(100) == [100]
 
 def test_withdraw_300_usd():
-    assert withdraw(300) == [200, 100]
+    atm = ATM()
+    assert atm.withdraw(300) == [200, 100]
 
 def test_withdraw_434_usd():
-    assert withdraw(434) == [200, 200, 20, 10, 2, 2]
+    atm = ATM()
+    assert atm.withdraw(434) == [200, 200, 20, 10, 2, 2]
 
 def test_print_withdraw_434_usd():
-    assert print_withdraw(434) == "2 bills of 200.\n1 bill of 20.\n1 bill of 10.\n2 coins of 2.\n"
+    atm = ATM()
+    assert atm.print_withdraw(434) == "2 bills of 200.\n1 bill of 20.\n1 bill of 10.\n2 coins of 2.\n"
