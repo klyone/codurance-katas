7d1f8df9488f49e59ff76874b9fdfbd2ce725f01
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2023-01-25 00:36:04 +0100

    ATM: Rename test (remove the usd suffix)

--- ATM/test_atm.py
+++ ATM/test_atm.py
@@ -6,22 +6,22 @@ from atm import *
 def test_nothing():
     pass
 
-def test_withdraw_200_usd():
+def test_withdraw_200():
     atm = ATM()
     assert ATM.extract_money_quantity(atm.withdraw(200)) == [200]
 
-def test_withdraw_100_usd():
+def test_withdraw_100():
     atm = ATM()
     assert ATM.extract_money_quantity(atm.withdraw(100)) == [100]
 
-def test_withdraw_300_usd():
+def test_withdraw_300():
     atm = ATM()
     assert ATM.extract_money_quantity(atm.withdraw(300)) == [200, 100]
 
-def test_withdraw_434_usd():
+def test_withdraw_434():
     atm = ATM()
     assert ATM.extract_money_quantity(atm.withdraw(434)) == [200, 200, 20, 10, 2, 2]
 
-def test_print_withdraw_434_usd():
+def test_print_withdraw_434():
     atm = ATM()
     assert atm.print_withdraw(434) == "2 bills of 200.\n1 bill of 20.\n1 bill of 10.\n2 coins of 2.\n"
