e69c7a9a2b4c8c6c2351d2cae04e55ec78cfe593
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2023-01-30 08:07:15 +0100

    ShoppingCart: Empty shopping cart logic & test

--- ShoppingCart/shoppingcart.py
+++ ShoppingCart/shoppingcart.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+class ShoppingCart:
+    def __init__(self):
+        pass
+    def printShoppingCart(self):
+        print("--------------------------------------------")
+        print("| Product name | Price with VAT | Quantity |")
+        print("| -----------  | -------------- | -------- |")
+        print("|------------------------------------------|")
+        print("| Promotion:                               |")
+        print("--------------------------------------------")
+        print("| Total productos: 0                       |")
+        print("| Total price: 0.00 €                      |")
+        print("--------------------------------------------")
--- ShoppingCart/test_shoppingcart.py
+++ ShoppingCart/test_shoppingcart.py
@@ -2,6 +2,25 @@
 
 import pytest
 from shoppingcart import *
+from unittest.mock import call
 
-def test_check_shopping_cart_empty():
+def testCreateShoppingCart():
     cart = ShoppingCart()
+
+def testPrintEmptyShoppingCart(mocker):
+    cart = ShoppingCart()
+    mocker_print = mocker.patch("builtins.print")
+    cart.printShoppingCart()
+    mocker_print.assert_has_calls(
+        [
+            call("--------------------------------------------"),
+            call("| Product name | Price with VAT | Quantity |"),
+            call("| -----------  | -------------- | -------- |"),
+            call("|------------------------------------------|"),
+            call("| Promotion:                               |"),
+            call("--------------------------------------------"),
+            call("| Total productos: 0                       |"),
+            call("| Total price: 0.00 €                      |"),
+            call("--------------------------------------------"),
+        ]
+    )
