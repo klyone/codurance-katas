6bf22f0990fc9fc288bd69b802acfd837fc4a40b
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2023-01-30 08:42:05 +0100

    ShoppingCart: Add one product to the Shopping cart and show the status.

--- ShoppingCart/shoppingcart.py
+++ ShoppingCart/shoppingcart.py
@@ -1,15 +1,72 @@
 #!/usr/bin/env python3
 
+import math
+
+# Get the original implementation at:
+# https://realpython.com/python-rounding/#:~:text=To%20implement%20the%20%E2%80%9Crounding%20up,equal%20to%20a%20given%20number.
+def round_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+
+class Product:
+    def __init__(self, name, price, revenue, vat):
+        self.name = name
+        self.price = price
+        self.revenue = revenue
+        self.vat = vat
+
+    def getName(self):
+        return self.name
+
+    def getPriceWithRevenue(self):
+        return round_up(self.price + (self.price*(self.revenue/100.0)), 2)
+
+    def getFinalPrice(self):
+        p = self.getPriceWithRevenue()
+        return round_up(p + (p*(self.vat/100.0)), 2)
+
+
 class ShoppingCart:
     def __init__(self):
-        pass
+        self.products = []
+
+    def __getNumberOfSpecificProduct(self, product_name):
+        n = 0
+
+        for p in self.products:
+            if p.getName() == product_name:
+                n = n + 1
+
+        return n
+
     def printShoppingCart(self):
+        total_products = 0
+        total_price = 0.00
+        processed_products = []
+
         print("--------------------------------------------")
         print("| Product name | Price with VAT | Quantity |")
         print("| -----------  | -------------- | -------- |")
+
+        for p in self.products:
+            product_name = p.getName()
+
+            if not product_name in processed_products:
+                product_final_price = p.getFinalPrice()
+                number_of_products = self.__getNumberOfSpecificProduct(product_name)
+                ticket_line = "| " + str(product_name) + "      | " + str("{0:.2f}".format(product_final_price)) +" €         | " + str(number_of_products) + "        |"
+                print(ticket_line)
+                processed_products.append(product_name)
+                total_price = total_price + (product_final_price * number_of_products)
+
+            total_products = total_products + 1
+
         print("|------------------------------------------|")
         print("| Promotion:                               |")
         print("--------------------------------------------")
-        print("| Total productos: 0                       |")
-        print("| Total price: 0.00 €                      |")
+        print("| Total productos: " + str(total_products) + "                       |")
+        print("| Total price: " + str("{0:.2f}".format(total_price)) + " €                      |")
         print("--------------------------------------------")
+
+    def addItem(self, product):
+        self.products.append(product)
--- ShoppingCart/test_shoppingcart.py
+++ ShoppingCart/test_shoppingcart.py
@@ -24,3 +24,24 @@ def testPrintEmptyShoppingCart(mocker):
             call("--------------------------------------------"),
         ]
     )
+
+def testAddItemToShoppingCartAndPrint(mocker):
+    cart = ShoppingCart()
+    product = Product("Iceberg", 1.55, 15, 21)
+    mocker_print = mocker.patch("builtins.print")
+    cart.addItem(product)
+    cart.printShoppingCart()
+    mocker_print.assert_has_calls(
+        [
+            call("--------------------------------------------"),
+            call("| Product name | Price with VAT | Quantity |"),
+            call("| -----------  | -------------- | -------- |"),
+            call("| Iceberg      | 2.17 €         | 1        |"),
+            call("|------------------------------------------|"),
+            call("| Promotion:                               |"),
+            call("--------------------------------------------"),
+            call("| Total productos: 1                       |"),
+            call("| Total price: 2.17 €                      |"),
+            call("--------------------------------------------"),
+        ]
+    )
